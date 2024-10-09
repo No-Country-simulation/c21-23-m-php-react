832fefbab6c92d0f8360af4982f03e3cdc5b2933
Kelvin Suarez <k_suarez22@hotmail.com>
2024-10-09 12:00:22 -0400

    Primera estructura react para CalidHogar

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import { Route, Router, Navigate, useNavigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='root'/>
+      <h1>Bienvenidos a CalidHogar</h1>
+      <h2>Empecemos a trabajar!!</h2>
+    </div>
+  );
+}
+
+export default App;

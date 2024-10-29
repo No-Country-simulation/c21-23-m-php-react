8efa2e6bf67d014165ddaee83adfb8d022e9f447
ttaxyy <ttaxyy@proton.me>
2024-10-29 15:56:02 -0300

    Registro + adoptar

--- calidhogar/src/App.jsx
+++ calidhogar/src/App.jsx
@@ -2,7 +2,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Main from './Main/Main'
+import Registro from './Registro/Registro';
 import Footer from './components/Footer/Footer';
+import Adoptar from './Adoptar/Adoptar';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/registrarse" element={<Registro />}></Route>
+        <Route path="/adoptar" element={<Adoptar />}></Route>
       </Routes>
       <Footer />
     </>
---
+++ calidhogar/src/Registro/Registro.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Registro.css"
+
+export default function Registro() {
+  return (
+    <div id="registro">
+        <div>
+            <div>
+                <h2>Ellos te esperan con ansias.</h2>
+                <p>¡Forma parte  de la familia de CalidHogar!</p>
+                <form>
+                    <input type="text" placeholder='Nombre' />
+                    <input type="text" placeholder='Apellido' />
+                    <input type="email" placeholder='Correo Electrónico' />
+                    <input type="password" placeholder='Contraseña'/>
+                    <input type="password" placeholder='Confirmar Contraseña' />
+                    <input type="submit"/>
+                </form>
+            </div>
+        </div>
+        <div id='imagen-registro' />
+    </div>
+  )
+}
